5e73ee8609d7bbe5c718ad60552fa8d8ea5054b9
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-21 23:04:20 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Signup from './components/Signup';
-import {Router, BrowserRouter} from 'react-router-dom';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+    <Router>
       <Navbar />
-      <Login />
-      <Signup />
-    </BrowserRouter>
+      <Switch>
+        <Route path = '/login'> <Login /> </Route>
+        <Route path = '/signup'> <Signup /> </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Signup = () => {
   return (
     <div>
-     
+     sign up here
     </div>
   )
 }
