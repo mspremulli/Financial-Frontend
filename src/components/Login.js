ba710294992b2624581d8bf6f6c8e79986869b29
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-23 18:41:38 -0500

    added login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const Login = () => {
-  return (
-    <div>
-    login here
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  //runs the validators in the backend when login is clicked
+ 
+   const Login = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setEmail(data.get('email'));
+    setPassword(data.get('password'));
+    // login({email, password});
+    console.log(email, password);
+  } 
+  //input form to submit login email & password
+  return(
+    <div className = "loginForm">
+      <form onSubmit =  {(e) => Login(e)}>
+        <input
+          type = "text"
+          id = "email"
+          name = 'email'
+          placeholder = "Your email here"
+          required
+        />
+        <input
+          type = "text"
+          id = "password"
+          name = 'password'
+          placeholder = "Enter password"
+          required
+        />
+        <br />
+        <button >
+          Login
+        </button>
+      </form>
     </div>
-  )
+    )
+  
 }
 
 export default Login;
