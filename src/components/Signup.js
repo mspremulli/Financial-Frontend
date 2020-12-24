1213e1da28742b123362ba8d06feeff3d54a99cf
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-24 15:48:58 -0500

    added signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Signup = () => {
-  return (
-    <div>
-     sign up here
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  //runs the validators in the backend to register a user
+   const handleSignup = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setEmail(data.get('email'));
+    setPassword(data.get('password'));
+    console.log(email, password);
+    
+  } 
+  //input form to submit login email & password
+  return(
+    <div className = "loginForm">
+      <form onSubmit =  {(e) => handleSignup(e)}>
+        <input
+          type = "text"
+          id = "email"
+          name = 'email'
+          placeholder = "Your email"
+          required
+        />
+        <input
+          type = "text"
+          id = "password"
+          name = 'password'
+          placeholder = "Enter password"
+          required
+        />
+        <br />
+        <button >
+          Sign up
+        </button>
+      </form>
     </div>
-  )
+    )
+  
 }
 
 export default Signup;
