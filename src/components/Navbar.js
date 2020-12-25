1a0d4e223cb367328ea76a0f228ed2ddacacb9c9
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-25 12:06:39 -0500

    login/navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import {Link} from 'react-router-dom';
 const Navbar = (props) => {
     return(
         <div className = "navbar">
-          <h1 className=''>
+          <h1 className='left'>
             <Link to='/'>Financial Fraud Reporter</Link>
           </h1> 
-          <h4 className=''>
+          <h4 className='right'>
             <Link to = '/login'>Login</Link>
           </h4>
-          <h4 className=''>
+          <h4 className='right'>
             <Link to = '/signup'>Sign Up</Link>
           </h4>
         </div>
